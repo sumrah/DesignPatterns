e2f6f89d58d3b04defb82324d5be21d7c76b9f0b
Sumrah_14 <60218191+sumrah@users.noreply.github.com>
2024-11-27 13:53:05 -0700

    Add files via upload

---
+++ saga-User-Service/src/test/java/com/nagarro/handson/SagaUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nagarro.handson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SagaUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
